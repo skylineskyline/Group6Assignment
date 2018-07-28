0256706bc03f742e339c495cd0b9e22cae24fa10
skylineskyline <emailaddis@gmail.com>
2018-07-28 08:37:04 -0600

    Add mainLogic and sql

--- Group6Assignment/Main/clsMainLogic.cs
+++ Group6Assignment/Main/clsMainLogic.cs
@@ -9,5 +9,32 @@ namespace Group6Assignment.Main
     public class clsMainLogic
     {
 
+
+        /// <summary>
+        /// This method calculates total cost user selected.
+        /// </summary>
+        public void CalculateTotalItemsCost()
+        {
+
+        }
+
+
+
+        /// <summary>
+        /// This method saves Invoice items user input.
+        /// </summary>
+        public void EditInvoice()
+        {
+
+        }
+
+
+        /// <summary>
+        /// This method deletes Invoice items user input from database.
+        /// </summary>
+        public void DeleteInvoice()
+        {
+
+        }
     }
 }
--- Group6Assignment/Main/clsMainSQL.cs
+++ Group6Assignment/Main/clsMainSQL.cs
@@ -9,15 +9,19 @@ namespace Group6Assignment.Main
     public class clsMainSQL
     {
         /// <summary>
-        /// This method create new Invoice.
+        /// This method is for create new Invoice statement.
         /// </summary>
         /// <returns></returns>
-        public string CreateNewInvoice(DateTime date, string tot)
+        public string CreateNewInvoice(DateTime date)
         {
-            return "INSERT INTO ItemDesc";
+            return "INSERT INTO ItemDesc (InvoiceDate) VALUES (#" + date + "#)";
         }
 
 
+        /// <summary>
+        /// This method is search for Items from ItemDesc
+        /// </summary>
+        /// <returns></returns>
         public string SearchInventory()
         {
             return "SELECT ItemCode, ItemDesc, Cost FROM ItemDesc";
