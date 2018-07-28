65b61e95c143e93e8918321b2429da4dbdfe0aa3
skylineskyline <emailaddis@gmail.com>
2018-07-28 09:50:34 -0600

    .

--- Group6Assignment/Main/wndMain.xaml.cs
+++ Group6Assignment/Main/wndMain.xaml.cs
@@ -19,9 +19,74 @@ namespace Group6Assignment.Main
     /// </summary>
     public partial class wndMain : Window
     {
+        /// <summary>
+        /// Class that manages creating, delete, editing, and calculate Invoice.
+        /// </summary>
+        private clsMainLogic clsMainLogic;
+
+
+
+
+
         public wndMain()
         {
             InitializeComponent();
+        }
+
+
+
+        /// <summary>
+        /// Event handler method for create new invoice.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BcreateInvoice_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Event handler method for edit invoice user created.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BeditInvoice_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Event handler method for delete invoice user created.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BdeleteInvoice_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Event handler method for add item user clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BaddItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        /// <summary>
+        /// Event handler method for save item(s) user selected.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BsaveInvoice_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
